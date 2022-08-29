86d00ad13cd2d6baeae76b0cdb46d2c80fd0f178
Aaron Renner <arennerwv@gmail.com>
2022-08-29 09:49:12 -0400

    handler documentation

--- kong/plugins/lce-route-by-header/handler.lua
+++ kong/plugins/lce-route-by-header/handler.lua
@@ -4,8 +4,8 @@ local lce_cache = require "kong.plugins.lce-route-by-header.helpers".cache
 local combine_paths = require "kong.plugins.lce-route-by-header.helpers".combine_paths
 local combine_querys = require "kong.plugins.lce-route-by-header.helpers".combine_query_strings
 local lce_parser = require "kong.plugins.lce-route-by-header.lce-parser"
-local kong   = kong
-local ngx    = ngx
+local kong = kong
+local ngx = ngx
 
 -- Required Kong values
 local LCE_RouteByHeader = {}
@@ -13,7 +13,9 @@ LCE_RouteByHeader.PRIORITY = 751
 
 -- runs in the 'access_by_lua_block'
 function LCE_RouteByHeader:access(config)
-  -- Get values for processing
+  --
+  -- Start of LCE Location ID Lookup
+  -- Getters/Setters for processing
   local parsingClockStart = os.clock()
   local debug = config.debug
   local headers = kong.request.get_headers()
@@ -28,13 +30,13 @@ function LCE_RouteByHeader:access(config)
     ngx.log(ngx.INFO, "Registry value from request  "..registryValue)
     ngx.log(ngx.INFO, "Parsing took "..os.clock()-parsingClockStart.." CPU seconds")
   end
-  ---
-  -- TODO: here do cache lookup for the URL
+
+  -- 
+  -- Start LCE Store Registry Lookup
+  -- Getters/Setters for processing 
   local lookupClockStart = os.clock()
-  local upstreamUrl, err = lce_cache(config, registryValue)
   -- Lookup the registryValue in the registry
-  -- Currently this function below is depricated as the cache should do everything
-  -- local upstreamUrl, err = lce_lookup(config, registryValue)
+  local upstreamUrl, err = lce_cache(config, registryValue)
   -- Error and debugging on this lookup
   if err then
     kong.response.exit(config.error_response_status_code, { message = "[LCE] " .. err })
@@ -43,18 +45,21 @@ function LCE_RouteByHeader:access(config)
     ngx.log(ngx.INFO, "Route from registry "..upstreamUrl)
     ngx.log(ngx.INFO, "Lookup took "..os.clock()-lookupClockStart.." CPU seconds")
   end
-  -- Parse the URL for component table
+
+  --
+  -- Start of LCE Upstream Redirection with Kong
+  -- Setters/Getters for processing
   local splitUrl = parse_url(upstreamUrl)
-  -- Setup service information virtually
   local upstream_query = combine_querys(splitUrl.query, kong.request.get_raw_query())
   local upstream_path = combine_paths(splitUrl.path, kong.request.get_path())
+  -- Error and debugging on this lookup
   if debug then
     ngx.log(ngx.INFO, "Path ["..tostring(splitUrl.path).."] + ["..kong.request.get_path().."] = "..(upstream_path))
     ngx.log(ngx.INFO, "Query ["..tostring(splitUrl.query).."] + ["..kong.request.get_raw_query().."] = "..(upstream_query))
     ngx.log(ngx.INFO, "Host: "..(splitUrl.host))
     ngx.log(ngx.INFO, "Port: "..tostring(splitUrl.port))
   end
-  -- TODO: check for forward slash on new and old path to ensure always has slash
+  -- Build the new Kong Service
   kong.service.request.set_path(upstream_path)
   kong.service.request.set_raw_query(upstream_query)
   kong.service.set_target(splitUrl.host, splitUrl.port)
